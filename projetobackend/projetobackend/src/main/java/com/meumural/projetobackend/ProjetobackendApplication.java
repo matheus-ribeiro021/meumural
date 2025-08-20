cc21ca01aee2761a270b6b1f4bbf907a9f471795
ADS 2024.1M <36129382024.1M@senacrjedu.local>
2025-08-20 11:01:24 -0300

    Estrutura inicial do projeto Meu Mural

---
+++ projetobackend/projetobackend/src/main/java/com/meumural/projetobackend/ProjetobackendApplication.java
@@ -0,0 +1,13 @@
+package com.meumural.projetobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobackendApplication.class, args);
+	}
+
+}
---
+++ projetobackend/projetobackend/src/test/java/com/meumural/projetobackend/ProjetobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meumural.projetobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
